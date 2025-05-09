0ee70938a32617a6e06a28f098a09f0de44cf08f
Seaf <Seaftalal2017@gmail.com>
2025-05-09 18:08:01 +0300

    Here we add a new blank page - Weather Page -

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:alrayan/home.dart';
+import 'package:alrayan/pages/home.dart';
+import 'package:alrayan/pages/weatherPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const Home(),
+    home: const Weather(),
   ));
 }
 
---
+++ lib/pages/weatherPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatelessWidget {
+  const Weather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
